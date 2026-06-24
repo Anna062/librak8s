e504127d8fa0b1e370918bc8e7143a8962a75024
Anna062 <dickoanna6@gmail.com>
2026-06-24 11:30:26 +0200

    fix: deploy front

--- librak8s-react/src/shared/lib/axios.ts
+++ librak8s-react/src/shared/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL as string,
+  baseURL: import.meta.env.VITE_API_URL as string ?? "/api",
 })
 
 apiClient.interceptors.request.use((config) => {
